0a000d6facb99049b4757e5012ed6e46bf31b8ae
Richard <Richard@DESKTOP-J6MDR3C>
2022-05-18 08:07:46 +0200

    Add project files.

---
+++ AOC1/Program.cs
@@ -0,0 +1,56 @@
+﻿
+class FindNumbers
+{
+    static void Main()
+
+    {
+
+        string path = @"C:\Users\Richard\Downloads\input.txt";
+
+        int count = 0;
+        int counter = 0;
+        var lines = File.ReadLines(path).ToArray();
+
+        int[] array = new int[lines.Length];
+
+
+        foreach (var line in lines)
+        {
+
+            int numberA = Int32.Parse(line);
+
+            array[count++] = numberA;
+
+        }
+
+        /*
+
+        //Day1:1 solution
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            if (array[i - 1] < array[i])
+                counter++;
+        }
+
+        Console.WriteLine("The number of occurances with greater numbers are: " + counter);
+
+        */
+
+        //Day1:2 solution
+
+        for (int i = 3; i < lines.Length; i++)
+        {
+            if (  (array[i-3]+array[i-2]+array[i-1]) < (array[i-2]+array[i-1]+array[i]))
+                counter++;
+        }
+
+        Console.WriteLine("The number of occurances with greater sequence than last are " + counter);
+
+    }
+
+}
+
+
+
+
